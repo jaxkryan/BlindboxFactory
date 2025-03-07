f7cc0c75309d853da80732569f0c62ea2bbf6bb3
Twannnnnn <hkt93885@gmail.com>
2025-03-07 13:33:01 +0700

    add some loosing code and prefab from toanbranch

---
+++ Assets/Script/BlindBoxMachine/BlindBoxAmountPerCraft.cs
@@ -0,0 +1,22 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlindBoxAmountPerCraft : MonoBehaviour
+{
+    [SerializeField]
+    public Slider slider;
+    [SerializeField]
+    public TMP_Text valueText;
+
+    void Start()
+    {
+        slider.onValueChanged.AddListener(UpdateValueText);
+        UpdateValueText(slider.value); // Initialize the text
+    }
+
+    void UpdateValueText(float value)
+    {
+        valueText.text = ((int)value).ToString(); // Ensure it displays as an integer
+    }
+}
