be34d787f3bdb615e5d0bc982037907b43af7af2
Twannnnnn <hkt93885@gmail.com>
2025-04-02 11:22:36 +0700

    Reapply "Merge branch 'sprint-04' of https://github.com/jaxkryan/LastAssignment into sprint-04"
    This reverts commit 5f56f31f3dbcbd2cf922766ccce4e9e7e7fce428.

---
+++ Assets/Script/ServerSaving/Player.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private string id;
+    private DateTime lastLogin;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
