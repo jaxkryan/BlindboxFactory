e115c4425ff9a633a3200a70ef169bf50dd16eba
iamapant <longdhhe160008@fpt.edu.vn>
2025-04-10 11:54:08 +0700

    fix create commission

--- Assets/Script/Controller/CommissionController.cs
+++ Assets/Script/Controller/CommissionController.cs
@@ -18,12 +18,13 @@ namespace Script.Controller {
         [SerializeField] private bool _forAllProducts;
         [ConditionalField(nameof(_forAllProducts), inverse: true)] 
         [SerializeField] private List<BoxTypeName> _commissionedProducts;
-        [FormerlySerializedAs("_numberOfCommissions")] [SerializeField] private int _numberOfCommissionsPerItem;
-        [SerializeField] private float _amountModifierForNextProduct;
-        [SerializeField] private Vector2 _bonusRange;
-        [SerializeField][Min(0)] private int _maximumTotalCommissions;
-        [SerializeField][Min(0)] private int _baseCommission;
-        [SerializeField][Min(0)] private int _expireHours;
+        [FormerlySerializedAs("_numberOfCommissions")] [SerializeField][Min(1)] private int _numberOfCommissionsPerItem;
+        [SerializeField][Min(0.01f)] private float _amountModifierForNextProduct = 1f;
+        [SerializeField][Min(0.1f)] private Vector2 _bonusRange;
+        [SerializeField][Min(1)] private int _maximumTotalCommissions;
+        [SerializeField][Min(1)] private int _baseCommission;
+        [SerializeField][Min(1)] private int _expireHours;
+        [SerializeField] private bool _log = true;
 
         public ReadOnlyCollection<Commission.Commission> Commissions { get => _commissions.AsReadOnly(); }
         private List<Commission.Commission> _commissions = new();
@@ -32,6 +33,7 @@ namespace Script.Controller {
         public event Action<Commission.Commission> onCommissionCompleted = delegate { };
 
         public HashSet<Commission.Commission> CreateCommissions() {
+            if (_log) Debug.Log($"Creating commissions");
             var bb = _forAllProducts
                 ? Enum.GetValues(typeof(BoxTypeName)).Cast<BoxTypeName>().ToList()
                 : _commissionedProducts;
@@ -40,33 +42,45 @@ namespace Script.Controller {
             
             List<Commission.Commission> commissions = new ();
             foreach (var boxType in bb) {
+                if (boxType == BoxTypeName.Null) continue;
+                if (_log) Debug.Log($"Creating commissions for {boxType}");
                 List<Commission.Commission> boxCommissions = new ();
                 var data = BoxTypeManager.Instance.GetBoxData(boxType);
                 
                 //Find the number of box sold the previous day
                 var prevSales = controller.SaleData.Where(s => s.BoxTypeName == boxType && s.DateTime >= DateTime.Now.AddDays(-1)).ToList();
                 var prevBoxSold = prevSales.Sum(s => s.Amount);
-                var prevBoxSoldPerCommission = prevBoxSold / prevSales.Count;
                 //If sold 0 box or fewer than base, use the base number instead
-                if (prevBoxSoldPerCommission < _baseCommission) prevBoxSold = _baseCommission;
+                if (prevBoxSold < _baseCommission) prevBoxSold = _baseCommission;
+                var prevBoxSoldPerCommission = prevBoxSold / (prevSales.Count == 0 ? 1 : prevSales.Count);
+                if (_log) Debug.Log($"PrevBoxSold{prevBoxSold}\tPrevSalesCount{prevSales.Count}\tPrevBoxSoldPerCommission: {prevBoxSoldPerCommission}");
+
                 //Use the number of box sold as medium, find upper and lower number of box should be sold based on amount modifier
-                var upperBound = _numberOfCommissionsPerItem / 2 + (_numberOfCommissionsPerItem % 2 == 0 ? 0 : 1);
                 var lowerBound = _numberOfCommissionsPerItem / 2;
+                var upperBound = lowerBound + _numberOfCommissionsPerItem + (_numberOfCommissionsPerItem % 2 == 0 ? 0 : 1);
+                if (_log) Debug.Log($"Commission per item bounds {lowerBound} - {upperBound}");
+
                 List<int> newCommissionsCount = new();
                 for (int i = 0; i < upperBound; i++) {
                     var modifierNumber = (int)Math.Floor(prevBoxSoldPerCommission * _amountModifierForNextProduct) - prevBoxSoldPerCommission;
                     newCommissionsCount.Add(prevBoxSoldPerCommission + modifierNumber * i);
+
+                    if (_log) Debug.Log($"Adding upper bound commission to counter. Amount of item: {prevBoxSoldPerCommission + modifierNumber * i}");
                 }
                 for (int i = 0; i < lowerBound; i++) {
                     var modifierNumber = (int)Math.Floor(prevBoxSoldPerCommission * _amountModifierForNextProduct) - prevBoxSoldPerCommission;
                     newCommissionsCount.Add(prevBoxSoldPerCommission - modifierNumber * i);
+                    
+                    if (_log) Debug.Log($"Adding upper bound commission to counter. Amount of item: {prevBoxSoldPerCommission - modifierNumber * i}");
                 }
                 //Calculate bonus 
                 List<(int CommissionsCount, long Base, long Bonus)> newCommissionPayout = new();
                 foreach (var count in newCommissionsCount) {
                     var basePrice = count * data.value;
-                    var bonus = (long)(basePrice * new Unity.Mathematics.Random().NextDouble(_bonusRange.x, _bonusRange.y));
+                    var bonus = (long)(basePrice * new Unity.Mathematics.Random((uint)System.DateTime.Now.Ticks).NextDouble(_bonusRange.x, _bonusRange.y));
                     newCommissionPayout.Add((count, basePrice, bonus));
+
+                    if (_log) Debug.Log($"Calculating commission payout for {count} {boxType}: Base: {basePrice}, Bonus: {bonus}");
                 }
                 //Create the commission
                 foreach (var payout in newCommissionPayout) {
@@ -79,6 +93,8 @@ namespace Script.Controller {
                         .Build();
                     reward.SetCommission(commission);
                     boxCommissions.Add(commission);
+                    
+                    if (_log) Debug.Log($"Creating commission: Item: {boxType}, Amount: {payout.CommissionsCount}, Base price: {payout.Base}, Bonus: {payout.Bonus}, Expired by: {DateTime.Now.AddHours(_expireHours)}, Reward: {reward.GetType().Name}");
                 }
                 
                 commissions.AddRange(boxCommissions);
