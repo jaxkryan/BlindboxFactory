c988b18ed07d07b243f31bb798d835e22f6a0323
lllooo121212 <loctqhe170736@fpt.edu.vn>
2025-04-18 15:28:44 +0700

    1/3 tutorial

--- Assets/Script/LocScripts/Tutorial/TutorialManager.cs
+++ Assets/Script/LocScripts/Tutorial/TutorialManager.cs
@@ -1,15 +1,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.Collections;
 
 public class TutorialManager : MonoBehaviour
 {
     public List<TutorialStep> steps;
     public RectTransform overlay;                 // overlay có lỗ tròn
     public DialogueManager dialogueManager;
-    public bool clickOverlay = true;              // nếu true → click toàn màn hình, false → chỉ click quanh vùng lỗ
+    //public bool clickOverlay = true;              // nếu true → click toàn màn hình, false → chỉ click quanh vùng lỗ
     public float clickRadius = 90f;               // bán kính click được khi clickOverlay = false
-
+    public RectTransform characterRect;
     private int currentStep = -1;
     private bool waitingForClick = false;
 
@@ -24,7 +25,9 @@ public class TutorialManager : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
-            if (clickOverlay)
+            var step = steps[currentStep];
+
+            if (step.clickOverlay)
             {
                 OnClickOverlay();
             }
@@ -47,6 +50,10 @@ public class TutorialManager : MonoBehaviour
         }
     }
 
+    public bool IsWaitingForClick()
+    {
+        return waitingForClick;
+    }
     public void StartTutorial()
     {
         currentStep = -1;
@@ -64,14 +71,24 @@ public class TutorialManager : MonoBehaviour
         }
 
         TutorialStep step = steps[currentStep];
-
+        characterRect.anchoredPosition = step.characterPosition;
         // Cập nhật vị trí overlay
         overlay.anchoredPosition = step.overlayCenterPosition;
         overlay.gameObject.SetActive(true);
-
+        // ✅ Nếu bật scale overlay → áp dụng
+        if (step.enableOverlayScale)
+        {
+            overlay.localScale = step.overlayScale;
+        }
+        else
+        {
+            overlay.localScale = Vector3.one;
+        }
         // Thoại + ảnh nhân vật
-        dialogueManager.StartDialogue(step.dialogueText, step.characterSprite);
 
+        characterRect.gameObject.SetActive(true);
+
+        dialogueManager.StartDialogue(step.dialogueText, step.characterSprite);
         // Sự kiện khởi đầu bước
         step.onStepStart?.Invoke();
 
@@ -84,7 +101,16 @@ public class TutorialManager : MonoBehaviour
 
         waitingForClick = false;
 
+        // Gọi sự kiện hoàn thành step nếu có
         steps[currentStep].onStepComplete?.Invoke();
+
+        // Đợi 0.5 giây rồi mới NextStep
+        StartCoroutine(WaitAndNextStep(0.5f));
+    }
+
+    IEnumerator WaitAndNextStep(float delay)
+    {
+        yield return new WaitForSeconds(delay);
         NextStep();
     }
 
