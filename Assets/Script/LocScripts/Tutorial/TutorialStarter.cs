3e141795346eb5d6c8f36dcf22645b14dc872c4f
Twannnnnn <hkt93885@gmail.com>
2025-04-22 23:46:03 +0700

    Merge Loc code

---
+++ Assets/Script/LocScripts/Tutorial/TutorialStarter.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class TutorialStarter : MonoBehaviour
+{
+    public TutorialManager tutorialManager;
+    public GameObject tutorialCanvas;  // ✅ Kéo TutorialCanvas vào đây
+
+    // Biến trạng thái đơn giản: false = chưa xong, true = đã hoàn thành
+    public bool hasCompletedTutorial ;
+
+    void Start()
+    {
+        if (!hasCompletedTutorial)
+        {
+            tutorialManager.StartTutorial();
+            
+        }
+        else
+        {
+            if (tutorialCanvas != null)
+            {
+                tutorialCanvas.SetActive(false);  // ✅ Tắt canvas nếu đã xong
+            }
+        }
+    }
+
+    public void IsTutorialCompleted()
+    {
+         hasCompletedTutorial = true;
+        tutorialCanvas.SetActive(false);
+    }
+}
