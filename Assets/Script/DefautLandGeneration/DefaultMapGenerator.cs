e11f37d08d0cb05bf3368605d4dff7ee5c2f0562
Twannnnnn <hkt93885@gmail.com>
2025-04-26 14:35:30 +0700

    temp push

--- Assets/Script/DefautLandGeneration/DefaultMapGenerator.cs
+++ Assets/Script/DefautLandGeneration/DefaultMapGenerator.cs
@@ -8,6 +8,8 @@ public class DefaultMapGenerator : MonoBehaviour
     public Tilemap tilemap;
     public Tile groundTile;
     public Tile portalTile;
+    public GameObject leftGroundPrefab;
+    public GameObject rightGroundPrefab;
 
     public int size = 32;
     public int portalsize = 4;
@@ -15,6 +17,8 @@ public class DefaultMapGenerator : MonoBehaviour
     void Start()
     {
         GenerateDefaultMap();
+        var tilePos = tilemap.gameObject.transform.position;
+        //SpawnGroundSides(Vector3Int.zero);
     }
 
     void GenerateDefaultMap()
@@ -42,5 +46,38 @@ public class DefaultMapGenerator : MonoBehaviour
 
         tilemap.CompressBounds();
         GameController.Instance.BuildNavMesh();
+    }
+
+    void SpawnGroundSides(Vector3Int tilePos)
+    {
+        Vector3 center = tilemap.CellToWorld(tilePos) + tilemap.tileAnchor;
+        float width = tilemap.layoutGrid.cellSize.x;
+        float height = tilemap.layoutGrid.cellSize.y;
+
+        // Key points
+        Vector3 left = center + new Vector3(-width / 2f, 0, 0);
+        Vector3 right = center + new Vector3(width / 2f, 0, 0);
+        Vector3 bottom = center + new Vector3(0, -height / 2f, 0);
+
+        // Intersection point
+        Vector3 intersection = new Vector3(center.x, left.y, 0);
+
+        // Top width for each ground piece
+        float halfWidth = Vector3.Distance(left, intersection);
+
+        // Midpoints
+        Vector3 leftMid = (left + intersection) / 2f;
+        Vector3 rightMid = (right + intersection) / 2f;
+
+        // Height of ground objects
+        float groundHeight = Vector3.Distance(intersection, bottom);
+
+        // LEFT ground
+        GameObject leftObj = Instantiate(leftGroundPrefab, leftMid - new Vector3(0, groundHeight / 2f, 0), Quaternion.identity, this.transform);
+        leftObj.transform.localScale = new Vector3(halfWidth, groundHeight, 1f);
+
+        // RIGHT ground
+        GameObject rightObj = Instantiate(rightGroundPrefab, rightMid - new Vector3(0, groundHeight / 2f, 0), Quaternion.identity, this.transform);
+        rightObj.transform.localScale = new Vector3(halfWidth, groundHeight, 1f);
     }
 }
