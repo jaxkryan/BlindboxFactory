010c0f12b95f30101ee505b75c10d61ab485e8fc
Twannnnnn <hkt93885@gmail.com>
2025-05-03 20:54:47 +0700

    Update CommissionReward.cs

--- Assets/Script/Commission/CommissionReward.cs
+++ Assets/Script/Commission/CommissionReward.cs
@@ -8,19 +8,25 @@ namespace Script.Controller.Commission {
         public override void Grant() {
             var boxController = GameController.Instance.BoxController;
             var resourceController = GameController.Instance.ResourceController;
-            _commission.Items.ForEach(i => {
-                if (boxController.TryGetAmount(i.Key, out var amount))
-                {
-                    if (amount < i.Value) return;
-                    boxController.TrySetAmount(i.Key, amount - i.Value);
-                }
-                else return;
-            });
-
-
-            if (resourceController.TryGetAmount(Resource.Gold, out var amount))
+            foreach (var i in _commission.Items)
             {
-                resourceController.TrySetAmount(Resource.Gold, amount + _commission.Price);
+                if (boxController.TryGetAmount(i.Key, out var bAmount))
+                {
+                    if (bAmount < i.Value)
+                        return;
+
+                    boxController.TrySetAmount(i.Key, bAmount - i.Value);
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+
+            if (resourceController.TryGetAmount(Resource.Gold, out var rAmount))
+            {
+                resourceController.TrySetAmount(Resource.Gold, rAmount + _commission.Price);
             }
         }
     }
